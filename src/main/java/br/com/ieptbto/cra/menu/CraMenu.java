9dd82d9a42305a3effc2b3d1dc0e817b77d5a1f0
Leandro <leferfsa@gmail.com>
2015-06-22 14:56:21 -0300

    ajustes

--- src/main/java/br/com/ieptbto/cra/menu/CraMenu.java
+++ src/main/java/br/com/ieptbto/cra/menu/CraMenu.java
@@ -32,61 +32,32 @@ public class CraMenu extends Panel {
 		String[] rolesPesquisar = { CraRoles.USER };
 
 		MenuItem menuLateral = menu.addItem("menuLateral", rolesPesquisar);
-		/**Menus arquivos*/
+		/** Menus arquivos */
 		menuLateral.addItem("BuscarArquivo", rolesPesquisar);
 		menuLateral.addItem("EnviarArquivo", rolesPesquisar);
 		menuLateral.addItem("ArquivosPage", rolesPesquisar);
-		
-		/**Menus titulos*/
+
+		/** Menus titulos */
 		menuLateral.addItem("MonitorarTitulos", rolesPesquisar);
 
-		/**Menus Relatorios*/
+		/** Menus Relatorios */
 		menuLateral.addItem("RelatorioSintetico", rolesIncluir);
 		menuLateral.addItem("RelatorioArquivosTitulos", rolesIncluir);
 
-	    /**Menus Admins*/
+		/** Menus Admins */
 		menuLateral.addItem("Batimento", rolesIncluir);
 		menuLateral.addItem("LiberarTituloConvenios", rolesIncluir);
-		menuLateral.addItem("ConfirmacaoRetorno",rolesIncluir);
-//	    menuLateral.addItem("TipoArquivoPage", rolesIncluir);
-	    menuLateral.addItem("CartoriosPage", rolesIncluir);
-	    menuLateral.addItem("InstituicoesPage", rolesIncluir);
-	    menuLateral.addItem("MunicipiosPage", rolesIncluir);
-	    menuLateral.addItem("TipoInstituicoesPage", rolesIncluir);
-	    menuLateral.addItem("UsuariosPage", rolesIncluir);
-	    
-	    /**Instrumento de Protesto*/
-	    menuLateral.addItem("InstrumentoDeProtesto", rolesIncluir);
-	    menuLateral.addItem("ImportarArquivoDePara", rolesIncluir);
-	}
+		menuLateral.addItem("ConfirmacaoRetorno", rolesIncluir);
+		// menuLateral.addItem("TipoArquivoPage", rolesIncluir);
+		menuLateral.addItem("CartoriosPage", rolesIncluir);
+		menuLateral.addItem("InstituicoesPage", rolesIncluir);
+		menuLateral.addItem("MunicipiosPage", rolesIncluir);
+		menuLateral.addItem("TipoInstituicoesPage", rolesIncluir);
+		menuLateral.addItem("UsuariosPage", rolesIncluir);
 
-	// private void adicionarMenuAgenda(Menu menu) {
-	// MenuItem menuPrincipal = menu.addItem("menuAgenda");
-	// menuPrincipal.addItem("atendimento");
-	// menuPrincipal.addItem("pesquisarAtendimento");
-	// // menuPrincipal.addItem("agendamento");
-	// }
-	//
-	// private void adicionarMenuClinico(Menu menu) {
-	// MenuItem menuPrincipal = menu.addItem("menuClinico");
-	// menuPrincipal.addItem("anamnese");
-	// menuPrincipal.addItem("consulta");
-	// menuPrincipal.addItem("historico");
-	// }
-	//
-	// private void adicionarMenuRelatorio(Menu menu) {
-	// MenuItem menuPrincipal = menu.addItem("menuRelatorio");
-	// menuPrincipal.addItem("relatorioAtendimento");
-	// menuPrincipal.addItem("relatorioAtendimentoTipoServico");
-	// menuPrincipal.addItem("relatorioAtendimentoTipoAtendimento");
-	// }
-	//
-	// private void criarMenuEntidade(MenuItem menu, String nome, String[]
-	// rolesIncluir, String[] rolesPesquisar) {
-	// MenuItem menuPrincipal = menu.addItem("sm" + nome);
-	// menuPrincipal.addItem("incluir" + nome, rolesIncluir);
-	// menuPrincipal.addItem("pesquisar" + nome, rolesPesquisar);
-	//
-	// }
+		/** Instrumento de Protesto */
+		menuLateral.addItem("InstrumentoDeProtesto", rolesIncluir);
+		menuLateral.addItem("ImportarArquivoDePara", rolesIncluir);
+	}
 
 }
