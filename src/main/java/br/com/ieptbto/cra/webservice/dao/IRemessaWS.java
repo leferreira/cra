14bca0737dc2f1a421cc85c94801c867ec97224a
Thasso Araújo <thassoaraaujo@gmail.com>
2016-03-11 16:21:16 -0300

    Ajuste

--- src/main/java/br/com/ieptbto/cra/webservice/dao/IRemessaWS.java
+++ src/main/java/br/com/ieptbto/cra/webservice/dao/IRemessaWS.java
@@ -15,157 +15,144 @@ import org.springframework.stereotype.Repository;
 @Repository
 @WebService
 public interface IRemessaWS {
-	
-	/**
-	 * 
-	 * @param nomeArquivo
-	 * @param login
-	 * @param senha
-	 * @param dados
-	 * @return
-	 */
-	@WebMethod(operationName = "remessa")
-	@GET
-	public String remessa(@WebParam(name = "user_arq") String nomeArquivo, @WebParam(name = "user_code") String login,
-	        @WebParam(name = "user_pass") String senha, @WebParam(name = "user_dados") String dados);
 
-	/**
-	 * 
-	 * @param login
-	 * @param senha
-	 * @return
-	 */
-	@WebMethod(operationName = "arquivosPendentesCartorio")
-	@GET
-	public String arquivosPendentesCartorio(@WebParam(name = "user_code") String login,
-	        @WebParam(name = "user_pass") String senha);
-	
-	
-	/**
-	 * 
-	 * @param nomeArquivo
-	 * @param login
-	 * @param senha
-	 * @return
-	 */
-	@WebMethod(operationName = "buscarRemessa")
-	@GET
-	public String buscarRemessa(@WebParam(name = "user_arq") String nomeArquivo, @WebParam(name = "user_code") String login,
-	        @WebParam(name = "user_pass") String senha);
+    /**
+     * 
+     * @param nomeArquivo
+     * @param login
+     * @param senha
+     * @param dados
+     * @return
+     */
+    @WebMethod(operationName = "remessa")
+    @GET
+    public String remessa(@WebParam(name = "user_arq") String nomeArquivo, @WebParam(name = "user_code") String login, @WebParam(name = "user_pass") String senha, @WebParam(name = "user_dados") String dados);
 
-	/**
-	 * 
-	 * @param nomeArquivo
-	 * @param login
-	 * @param senha
-	 * @return
-	 */
-	@WebMethod(operationName = "confirmacao")
-	@GET
-	public String confirmacao(@WebParam(name = "user_arq") String nomeArquivo, @WebParam(name = "user_code") String login,
-			@WebParam(name = "user_pass") String senha);
+    /**
+     * 
+     * @param login
+     * @param senha
+     * @return
+     */
+    @WebMethod(operationName = "arquivosPendentesCartorio")
+    @GET
+    public String arquivosPendentesCartorio(@WebParam(name = "user_code") String login, @WebParam(name = "user_pass") String senha);
 
-	/**
-	 * 
-	 * @param nomeArquivo
-	 * @param login
-	 * @param senha
-	 * @param dados
-	 * 
-	 * @return
-	 */
-	@WebMethod(operationName = "enviarRetorno")
-	@GET
-	public String enviarRetorno(@WebParam(name = "user_arq") String nomeArquivo, @WebParam(name = "user_code") String login,
-	        @WebParam(name = "user_pass") String senha, @WebParam(name = "user_dados") String dados);
+    /**
+     * 
+     * @param nomeArquivo
+     * @param login
+     * @param senha
+     * @return
+     */
+    @WebMethod(operationName = "buscarRemessa")
+    @GET
+    public String buscarRemessa(@WebParam(name = "user_arq") String nomeArquivo, @WebParam(name = "user_code") String login, @WebParam(name = "user_pass") String senha);
 
-	/**
-	 * 
-	 * @param nomeArquivo
-	 * @param login
-	 * @param senha
-	 * @return
-	 */
-	@WebMethod(operationName = "retorno")
-	@GET
-	public String retorno(@WebParam(name = "user_arq") String nomeArquivo, @WebParam(name = "user_code") String login,
-	        @WebParam(name = "user_pass") String senha);
-	
-	/**
-	 * 
-	 * @param nomeArquivo
-	 * @param login
-	 * @param senha
-	 * @param dados
-	 * 
-	 * @return
-	 */
-	@WebMethod(operationName = "enviarConfirmacao")
-	@GET
-	public String enviarConfirmacao(@WebParam(name = "user_arq") String nomeArquivo, @WebParam(name = "user_code") String login,
-	        @WebParam(name = "user_pass") String senha, @WebParam(name = "user_dados") String dados);
+    /**
+     * 
+     * @param nomeArquivo
+     * @param login
+     * @param senha
+     * @return
+     */
+    @WebMethod(operationName = "confirmacao")
+    @GET
+    public String confirmacao(@WebParam(name = "user_arq") String nomeArquivo, @WebParam(name = "user_code") String login, @WebParam(name = "user_pass") String senha);
 
-	/**
-	 * 
-	 * @param nomeArquivo
-	 * @param login
-	 * @param senha
-	 * @return
-	 */
-	@WebMethod(operationName = "buscarDesistenciaCancelamento")
-	@GET
-	public String buscarDesistenciaCancelamento(@WebParam(name = "user_arq") String nomeArquivo, @WebParam(name = "user_code") String login,
-	        @WebParam(name = "user_pass") String senha);
-	
-	/**
-	 * 
-	 * @param nomeArquivo
-	 * @param login
-	 * @param senha
-	 * @param dados
-	 * @return
-	 */
-	@WebMethod(operationName = "cancelamento")
-	@GET
-	public String cancelamento(@WebParam(name = "user_arq") String nomeArquivo, @WebParam(name = "user_code") String login,
-	        @WebParam(name = "user_pass") String senha, @WebParam(name = "user_dados") String dados);
+    /**
+     * 
+     * @param nomeArquivo
+     * @param login
+     * @param senha
+     * @param dados
+     * 
+     * @return
+     */
+    @WebMethod(operationName = "enviarRetorno")
+    @GET
+    public String enviarRetorno(@WebParam(name = "user_arq") String nomeArquivo, @WebParam(name = "user_code") String login, @WebParam(name = "user_pass") String senha, @WebParam(name = "user_dados") String dados);
 
-	/**
-	 * 
-	 * @param nomeArquivo
-	 * @param login
-	 * @param senha
-	 * @param dados
-	 * @return
-	 */
-	@WebMethod(operationName = "desistencia")
-	@GET
-	public String desistencia(@WebParam(name = "user_arq") String nomeArquivo, @WebParam(name = "user_code") String login,
-	        @WebParam(name = "user_pass") String senha, @WebParam(name = "user_dados") String dados);
+    /**
+     * 
+     * @param nomeArquivo
+     * @param login
+     * @param senha
+     * @return
+     */
+    @WebMethod(operationName = "retorno")
+    @GET
+    public String retorno(@WebParam(name = "user_arq") String nomeArquivo, @WebParam(name = "user_code") String login, @WebParam(name = "user_pass") String senha);
 
-	/**
-	 * 
-	 * @param nomeArquivo
-	 * @param login
-	 * @param senha
-	 * @param dados
-	 * @return
-	 */
-	@WebMethod(operationName = "autorizacaoCancelamento")
-	@GET
-	public String autorizacaoCancelamento(@WebParam(name = "user_arq") String nomeArquivo, @WebParam(name = "user_code") String login,
-	        @WebParam(name = "user_pass") String senha, @WebParam(name = "user_dados") String dados);
-	
-	
-	/**
-	 * 
-	 * @param login
-	 * @param senha
-	 * @param codigo apresentante
-	 * @return
-	 */
-	@WebMethod(operationName = "comarcasHomologadas")
-	@GET
-	public String comarcasHomologadas(@WebParam(name = "user_code") String login,
-	        @WebParam(name = "user_pass") String senha, @WebParam(name = "codapres") String codigoApresentante);
+    /**
+     * 
+     * @param nomeArquivo
+     * @param login
+     * @param senha
+     * @param dados
+     * 
+     * @return
+     */
+    @WebMethod(operationName = "enviarConfirmacao")
+    @GET
+    public String enviarConfirmacao(@WebParam(name = "user_arq") String nomeArquivo, @WebParam(name = "user_code") String login, @WebParam(name = "user_pass") String senha, @WebParam(name = "user_dados") String dados);
+
+    /**
+     * 
+     * @param nomeArquivo
+     * @param login
+     * @param senha
+     * @return
+     */
+    @WebMethod(operationName = "buscarDesistenciaCancelamento")
+    @GET
+    public String buscarDesistenciaCancelamento(@WebParam(name = "user_arq") String nomeArquivo, @WebParam(name = "user_code") String login, @WebParam(name = "user_pass") String senha);
+
+    /**
+     * 
+     * @param nomeArquivo
+     * @param login
+     * @param senha
+     * @param dados
+     * @return
+     */
+    @WebMethod(operationName = "cancelamento")
+    @GET
+    public String cancelamento(@WebParam(name = "user_arq") String nomeArquivo, @WebParam(name = "user_code") String login, @WebParam(name = "user_pass") String senha, @WebParam(name = "user_dados") String dados);
+
+    /**
+     * 
+     * @param nomeArquivo
+     * @param login
+     * @param senha
+     * @param dados
+     * @return
+     */
+    @WebMethod(operationName = "desistencia")
+    @GET
+    public String desistencia(@WebParam(name = "user_arq") String nomeArquivo, @WebParam(name = "user_code") String login, @WebParam(name = "user_pass") String senha, @WebParam(name = "user_dados") String dados);
+
+    /**
+     * 
+     * @param nomeArquivo
+     * @param login
+     * @param senha
+     * @param dados
+     * @return
+     */
+    @WebMethod(operationName = "autorizacaoCancelamento")
+    @GET
+    public String autorizacaoCancelamento(@WebParam(name = "user_arq") String nomeArquivo, @WebParam(name = "user_code") String login, @WebParam(name = "user_pass") String senha, @WebParam(name = "user_dados") String dados);
+
+    /**
+     * 
+     * @param login
+     * @param senha
+     * @param codigo
+     *            apresentante
+     * @return
+     */
+    @WebMethod(operationName = "comarcasHomologadas")
+    @GET
+    public String comarcasHomologadas(@WebParam(name = "user_code") String login, @WebParam(name = "user_pass") String senha, @WebParam(name = "codapres") String codigoApresentante);
 }
